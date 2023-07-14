c9bb1c667a688c2a043289973d9e25b8ea5b60cf
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-07-14 23:16:59 +0700

    Penambahan kode pada Controllers

--- app/core/App.php
+++ app/core/App.php
@@ -1,5 +1,17 @@
 <?php
-
 class App {
-    
+    public function __construct()
+    {
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if( isset($_GET['url']) )
+        {
+            $url = $_GET['url'];
+            return $url;
+        }
+    }
 }
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 
-require_once '/app/init.php';
+require_once 'app/init.php';
+$app = new App;
