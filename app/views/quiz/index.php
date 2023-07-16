5856c2dd4ceb7e645ce2fbd9a186bfed8c356092
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-07-16 23:38:39 +0700

    Refactor JS & CSS

--- app/views/quiz/index.php
+++ app/views/quiz/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quiz</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="app/src/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
@@ -93,7 +93,7 @@
             </div>
         </div>
     </main>
-    <script src="script.js"></script>
+    <script src="app/src/js/script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
